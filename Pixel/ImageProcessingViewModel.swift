fce1081c03a1dcdc6867c75d7f58711cbd40aef2
Nugroho Arief Widodo <nawidodo@gmail.com>
2025-02-26 21:47:33 +0700

    Fix temp adjustment algo

--- Pixel/ImageProcessingViewModel.swift
+++ Pixel/ImageProcessingViewModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bef86421700f51b0868dc240fcbb799ec60c1669092241bf625eb6658c77b2c8
-size 2027
+oid sha256:621865c3b048cda68a5f620ed57c0ccb19e2c6c8f206c19b0424846c60a1c2cd
+size 1785
--- Pixel/OpenCVWrapper.mm
+++ Pixel/OpenCVWrapper.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:418ae7bc050e39966ad9f1a533da7180d88ae75682d1c1507901405c0a2520d0
-size 1592
+oid sha256:9a0efdd021fc7961252fbd121f2b50444c6876a1689b2e2cb2ed3931bb049d0a
+size 1588
