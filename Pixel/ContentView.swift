44188a3aa5b2861beb378798c94069bb43f593c4
Nugroho Arief Widodo <nawidodo@gmail.com>
2025-02-27 21:25:37 +0700

    Add save to files function

--- Pixel/ContentView.swift
+++ Pixel/ContentView.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec4fefd254dac9b0c5421cfab41d3162cffe2a10a6361391108569d96d9f6f52
-size 4063
+oid sha256:e85112aa0a99b405b343cc39d9adc7e50fa95cb4f9d9ea0e6c0ea00d652b818a
+size 4526
--- Pixel/ImageProcessingViewModel.swift
+++ Pixel/ImageProcessingViewModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:833010cea4a5dfb63d9b613d3cfbf75ecd5538da6bd9cbeb1aa35dee14f555bd
-size 3313
+oid sha256:6453c8bbc2b7326e4e663759064113ea1264e0d9586367259dc6b11309ebdf6f
+size 4282
