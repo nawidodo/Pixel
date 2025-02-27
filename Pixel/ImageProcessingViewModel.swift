1f62da5b5bdd1d86d1c7f8ef45a18a4ca992225c
Nugroho Arief Widodo <nawidodo@gmail.com>
2025-02-27 22:45:39 +0700

    Add valid jpeg validator

--- Pixel/ImageProcessingViewModel.swift
+++ Pixel/ImageProcessingViewModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c071042af0b0f57674a62a315dd0d1df09379dc013fc8668425e23fa0732b20
-size 4307
+oid sha256:b36949409c8cfc8d91bc78fb925aab690684c155c1c5b3196e07a4c3c1ec2056
+size 4789
